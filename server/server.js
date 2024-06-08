fd1ce2c2d338d27fa713d0e84dee2abc5925ce44
AkshajJain <akshajj2004@gmail.com>
2024-06-08 14:58:37 +0530

    add frontend and backend code

---
+++ server/server.js
@@ -0,0 +1,33 @@
+// server.js
+require("dotenv").config()
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+const cors = require("cors");
+
+const todo = require("./routes/todo");
+const user = require("./routes/user")
+const { verifyToken } = require("./controllers/todo")
+
+connectDB();
+
+app.use(cors({ origin: true, credentials: true })); // added
+app.use(express.json({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => res.send("Server up and running"));
+
+// use routes
+app.use("/api/user", user);
+app.use(verifyToken);
+app.use("/api/todo", todo);
+// setting up port
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
